c39c0212b9e01c5b45e1883c4db3cd860c9ae0ff
Matias Lopez <matias.lopez.renato@gmail.com>
2024-06-18 19:31:06 -0400

    Agrega router dom

---
+++ src/router.jsx
@@ -0,0 +1,27 @@
+// src/router.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import PortfolioPage from './pages/PortfolioPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
+import FAQPage from './pages/FAQPage';
+
+function AppRouter() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/services" element={<ServicesPage />} />
+                <Route path="/portfolio" element={<PortfolioPage />} />
+                <Route path="/blog" element={<BlogPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/faq" element={<FAQPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
