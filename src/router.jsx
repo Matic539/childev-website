45625d5cf61d6d28082bba02d28a48b810346102
Matias Lopez <matias.lopez.renato@gmail.com>
2024-06-28 17:07:45 -0400

    Cambio de detalles en el navbar

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,8 @@
-// src/router.jsx
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TeamPage from './pages/TeamPage';
-// import ServicesPage from './pages/ServicesPage';
-// import PortfolioPage from './pages/PortfolioPage';
-// import BlogPage from './pages/BlogPage';
-// import ContactPage from './pages/ContactPage';
-// import FAQPage from './pages/FAQPage';
+import ServicesPage from './pages/ServicesPage';
 
 function AppRouter() {
     return (
@@ -14,11 +10,7 @@ function AppRouter() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/team" element={<TeamPage />} />
-                {/* <Route path="/services" element={<ServicesPage />} /> */}
-                {/* <Route path="/portfolio" element={<PortfolioPage />} /> */}
-                {/* <Route path="/blog" element={<BlogPage />} /> */}
-                {/* <Route path="/contact" element={<ContactPage />} /> */}
-                {/* <Route path="/faq" element={<FAQPage />} /> */}
+                <Route path="/services" element={<ServicesPage />} />
             </Routes>
         </Router>
     );
