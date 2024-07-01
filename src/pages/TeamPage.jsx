b0ea9cc0d148425af7d1fb06fefa531f52acce92
Matias Lopez <matias.lopez.renato@gmail.com>
2024-06-30 22:29:21 -0400

    agrego el navbar

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "../components/common/Navbar";
+// import Footer from '../components/common/Footer';
 
 
 function TeamPage() {
     return (
         <div id="next-section" className="next-section">
+            <Navbar />
             <h2>Welcome to the TeamPage</h2>
+            {/* <Footer /> */}
         </div>
     )
 }
