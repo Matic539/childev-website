002fc1e24f2b4f14832c398213c69184e7c03ea7
omelochino <falso5853@gmail..com>
2024-06-30 22:27:52 -0400

    navbar

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../components/common/Navbar';
 
 function ServicesPage() {
-    return (
-        <div>ServicesPage</div>
-    )
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
 }
 
 export default ServicesPage
